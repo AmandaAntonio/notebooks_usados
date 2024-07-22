44f27a77ef6e82bc8503ad45b84257abff558806
Amanda M Antonio <amanda.m.antonio@outlook.com.br>
2024-07-22 20:11:44 -0300

    analise de dados

--- src/analysis.py
+++ src/analysis.py
@@ -23,35 +23,33 @@ preco_medio = df['preco'].mean()
 print(f'O preço médio dos notebooks é', preco_medio)
 
 # 2.O notebook mais caro listado
-mais_caro = df[['nome', 'preco']]
-preco_caro = mais_caro.max()
-print(f'O notebook mais caro é ', preco_caro)
+mais_caro = df.loc[df['preco'].idxmax()]
+print(f'O notebook mais caro é o', mais_caro)
 
 # 3. O notebook mais barato listado
-mais_barato = df[['nome', 'preco']]
-preco_barato = mais_barato.min()
-print(f'O notebook mais barato é ', preco_barato)
+mais_barato = df.loc[df['preco'].idxmin()]
+print(f'O notebook mais baroto é o ', mais_barato)
 
 # 4.Preço médio por marca
 preco_medio_marca = df[['marca', 'preco']].groupby('marca').mean().sort_values(by='preco', ascending=False)
 print(preco_medio_marca)
 
-# 5.As marcas mais populares
+# 5.A quantidade de notebooks listados por marca
 marcas_pop = df['marca'].value_counts().sort_values(ascending=False)
 print(marcas_pop)
 
-# 6.A maioria dos notebooks listados por sistema operacional
+# 6.A quantidade de notebooks listados por sistema operacional
 sis_op_pop = df['sistema_operacional'].value_counts().sort_values(ascending=False)
 print(sis_op_pop)
 
-# 7.O preço médio dos notebooks são baseados em seu sistema operacional
+# 7.O preço médio dos notebooks baseados em seu sistema operacional
 preco_medio_sis_op = df[['preco', 'sistema_operacional']].groupby('sistema_operacional').mean().sort_values(by='preco', ascending=False)
 print(preco_medio_sis_op)
 
-# 8.Preço médio de notebooks por tamanho de RAM.
+# 8.Preço médio de notebooks por tamanho da RAM.
 preco_medio_ram = df[['preco', 'ram']].groupby('ram').mean().sort_values(by='preco', ascending=False)
 print(preco_medio_ram)
 
-# 9. Tamanho de memória RAM mais listadas
+# 9. Tamanho de memória RAM mais listada
 ram_pop = df['ram'].value_counts().sort_values(ascending=False)
 print(ram_pop)
